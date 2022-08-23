496dbd97f36ae7687aa72050ef60f858d9ce8c9e
BTDevPro <titus.barza@deutschefintechsolutions.de>
2022-08-23 19:18:00 +0300

    Create GET /contracts endpoint

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const {
+  Op: { ne },
+} = require("sequelize");
+
 const { sequelize } = require("./model");
+
 const { getProfile } = require("./middleware/getProfile");
+
 const app = express();
 app.use(bodyParser.json());
 app.set("sequelize", sequelize);
@@ -16,4 +22,21 @@ app.get("/contracts/:id", getProfile, async (req, res) => {
   if (!contract) return res.status(404).end();
   res.json(contract);
 });
+
+app.get("/contracts", getProfile, async (req, res) => {
+  const { Contract } = req.app.get("models");
+
+  const queryFieldName =
+    req.profile.type === "client" ? "ClientId" : "ContractorId";
+
+  const contractList = await Contract.findAll({
+    where: {
+      [queryFieldName]: req.profile.id,
+      status: { [ne]: "terminated" },
+    },
+  });
+
+  res.json(contractList);
+});
+
 module.exports = app;
